2c74ea145dcf703645241ae3cdf1605bcbc5a4ef
James Loh <jloh@squiz.net>
2015-04-20 14:39:00 +1000

    Updating dotfiles

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -1,9 +1,19 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+fi
+
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
-PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
+PATH="/usr/local/bin:~/bin:$(path_remove /usr/local/bin)"
+PATH="/usr/local/sbin:$PATH"
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 export PATH
+
+source ~/bin/bashmarks.sh
+
+if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases; fi
 
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
