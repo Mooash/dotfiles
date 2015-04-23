d471e10ad05b8a0fb0a3d400bf821c435d6b65cc
James Loh <jloh@squiz.net>
2015-04-24 09:41:03 +1000

    Moving interface over to the first variable

--- source/50_net.sh
+++ source/50_net.sh
@@ -5,4 +5,4 @@ alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias flush="dscacheutil -flushcache"
 
 # View HTTP traffic
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+alias httpdump="sudo tcpdump -i $1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
