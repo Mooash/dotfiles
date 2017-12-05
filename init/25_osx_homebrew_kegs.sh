41648c1a8030c9ada4500d948d1d5e518924b3e1
jloh <github@jloh.co>
2017-12-05 19:40:40 +1100

    separate out kegs and casks

---
+++ init/25_osx_homebrew_kegs.sh
@@ -0,0 +1,19 @@
+# OSX-only stuff. Abort if not OSX.
+is_osx || return 1
+
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
+
+# Ensure the cask kegs are installed.
+kegs=(
+  caskroom/cask
+  caskroom/drivers
+  caskroom/fonts
+  denji/nginx
+  homebrew/bundle
+  homebrew/core
+  homebrew/nginx
+  homebrew/services
+  homebrew/x11
+)
+brew_tap_kegs
--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -4,20 +4,6 @@ is_osx || return 1
 # Exit if Homebrew is not installed.
 [[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
 
-# Ensure the cask kegs are installed.
-kegs=(
-  caskroom/cask
-  caskroom/drivers
-  caskroom/fonts
-  denji/nginx
-  homebrew/bundle
-  homebrew/core
-  homebrew/nginx
-  homebrew/services
-  homebrew/x11
-)
-brew_tap_kegs
-
 # Hack to show the first-run brew-cask password prompt immediately.
 brew cask info this-is-somewhat-annoying 2>/dev/null
 
@@ -64,21 +50,4 @@ if (( ${#casks[@]} > 0 )); then
     brew cask install $cask
   done
   brew cask cleanup
-fi
-
-# Work around colorPicker symlink issue.
-# https://github.com/caskroom/homebrew-cask/issues/7004
-cps=()
-for f in ~/Library/ColorPickers/*.colorPicker; do
-  [[ -L "$f" ]] && cps=("${cps[@]}" "$f")
-done
-
-if (( ${#cps[@]} > 0 )); then
-  e_header "Fixing colorPicker symlinks"
-  for f in "${cps[@]}"; do
-    target="$(readlink "$f")"
-    e_arrow "$(basename "$f")"
-    rm "$f"
-    cp -R "$target" ~/Library/ColorPickers/
-  done
 fi
